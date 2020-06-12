83dfb4116a3069c72a6da80d1eeb5552d1eaeb4c
patrick <patricksinclair@hotmail.co.uk>
2020-06-12 16:37:01 +0100

    97pc

--- src/job_submit_script.sh
+++ src/job_submit_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#$ -N multispec-bigRun-99pc # job name
+#$ -N multispec-bigRun-97pc # job name
 #$ -V           # use all shell environment variables
 #$ -cwd         # run job in working directory
 # #$ -j y         # merge stdout and stderr to one file
