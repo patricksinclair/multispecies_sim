61603dbc3784c9f9a4e42324a7c3eb568b90abbb
Patrick <patricksinclair@hotmail.co.uk>
2020-11-24 12:54:19 +0000

    changed 15% n runs to 25 instead of 50

--- src/MultispeciesMain.java
+++ src/MultispeciesMain.java
@@ -7,15 +7,15 @@ public class MultispeciesMain {
         int nCores = Integer.parseInt(args[0]); //no. of cores used in parallel runs
         //changed nBlocks to 10, we'll do 10x10 runs to get more cores available
         //todo - for 14% resistance, make sure no. of cores is 10 (100 runs at a time)
-        //todo - for 15% resistance, make sure no. of cores is 25 (50 runs at a time)
+        //todo - for 15% resistance, make sure no. of cores is 25 (25 runs at a time)
         //todo - for 16% resistance, make sure no. of cores is 25 (25 runs at a time)
         //todo - this will require a different runID offset
         //no. of times a parallel run is performed.  total no. of runs = nCores * nBlocks.
         int nBlocks_14 = 10; //10 runs on each of 10 cores
-        int nBlocks_15 = 2; //2 runs on each of 25 cores
+        int nBlocks_15 = 1; //1 run on each of 25 cores
         int nBlocks_16 = 1; //1 run on each of 25 cores
         //runID_offset is used to adjust the run ID for successive runs, so that it starts at runID_offset instead of 0
-        //todo - make sure the runID_offset is correct (add on 100 for 14, 50 for 15%, 25 for 16%)
+        //todo - make sure the runID_offset is correct (add on 100 for 14, 25 for 15%, 25 for 16%) - check the end of the counters dataframe
         int runID_offset_14 = 400; //session 5
         int runID_offset_15 = 300; //session 5
         int runID_offset_16 = 175; //session 5
@@ -37,8 +37,8 @@ public class MultispeciesMain {
         Object[] params_16_resistant = new Object[]{"16_resistant"+date, 2.47772924764521, 0.7060073500033884};
 
         //BioSystem.getEventCountersAndRunPopulations(nCores, nBlocks_14, params_14_resistant, phase2_params, runID_offset_14);
-        //BioSystem.getEventCountersAndRunPopulations(nCores, nBlocks_15, params_15_resistant, phase2_params, runID_offset_15);
-        BioSystem.getEventCountersAndRunPopulations(nCores, nBlocks_16, params_16_resistant, phase2_params, runID_offset_16);
+        BioSystem.getEventCountersAndRunPopulations(nCores, nBlocks_15, params_15_resistant, phase2_params, runID_offset_15);
+        //BioSystem.getEventCountersAndRunPopulations(nCores, nBlocks_16, params_16_resistant, phase2_params, runID_offset_16);
         //BioSystem.timeToFailure(nCores, nBlocks, scale_93, sigma_93, folderID93);
     }
 
@@ -58,3 +58,31 @@ public class MultispeciesMain {
 //    Object[] params_10_resistant = new Object[]{"10_resistant"+date, 2.53711338, 0.6716238};
 //    Object[] params_12_resistant = new Object[]{"12_resistant"+date, 2.22826069, 0.84302476};
 }
+//    static void timeToNthMicrohabPhaseDiagram(Object[] params, int nCores, int microhab_lim){
+//        //this method is used to make a colour plot of the time taken to reach the Nth microhabitat as
+//        //a function of N* and r_det
+//        //save all the values for each of the runs in seperate dataframes, then do averaging etc later manually
+//        //todo - make sure the failure limit thing is set up correctly
+//
+//        String[] headers = new String[]{"n_thresh", "det_ratio", "time_to_n", "time_elapsed"};
+//        String results_directory = "/Disk/ds-sopa-personal/s1212500/multispecies-sims/biofilm_threshold_theory/"+params[0];
+//
+//        double duration = 1000.;
+//        int nMeasurements = 20;
+//        double N_thresh_min = 0., N_thresh_max = 1.5;
+//        double N_thresh_increment = (N_thresh_max - N_thresh_min)/nMeasurements;
+//        double r_det_ratio_min = 0., r_det_ratio_max = 1.5;
+//        double r_det_ratio_increment = (r_det_ratio_max - r_det_ratio_min)/nMeasurements;
+//        //ArrayList<DataBox[]> dataBoxes = new ArrayList<>();
+//
+//        for(int nt = 0; nt <= nMeasurements; nt++){
+//            for(int dr = 0; dr <= nMeasurements; dr++){
+//                double n_thresh = N_thresh_min + (nt*N_thresh_increment);
+//                double det_ratio = r_det_ratio_min + (dr*r_det_ratio_increment);
+//
+//                DataBox[] dataBoxes = timeToNthMicrohabPhaseDiagram_subroutine(nCores, duration, params, n_thresh, det_ratio, microhab_lim);
+//                String filename = String.format("mhLim-%d_N^-%.3f_rDet-%.3f", microhab_lim, n_thresh, det_ratio);
+//                Toolbox.writeTimeToNthMicrohabDataToFile(results_directory, filename, headers, dataBoxes);
+//            }
+//        }
+//    }
