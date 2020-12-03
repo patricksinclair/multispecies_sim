41ca420b76f38317582ea8af4950189825e0a4cc
Patrick <patricksinclair@hotmail.co.uk>
2020-12-03 12:03:04 +0000

    16% session 7

--- src/job_submit_script.sh
+++ src/job_submit_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#$ -N multispec-genoDistb-15pc-phase2-session7 # job name
+#$ -N multispec-genoDistb-16pc-phase2-session7 # job name
 #$ -V           # use all shell environment variables
 #$ -cwd         # run job in working directory
 # #$ -j y         # merge stdout and stderr to one file
